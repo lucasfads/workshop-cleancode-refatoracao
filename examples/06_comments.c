d5909a61542b84ac2144998a25b1591fb875fa0a
Gabriel Sistelos <gsistelos@proton.me>
2025-03-11 20:02:20 +0000

    chore: bloated and minimal comments examples

--- examples/06_comments.c
+++ examples/06_comments.c
@@ -9,3 +9,40 @@
 /*   Updated: 2025/03/09 13:46:41 by lucafern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+
+// function to print even numbers from an array of integers
+//   numbers -> a pointer to an array to iterate
+//   length  -> the length of the array
+void print_even_commented(int *numbers, size_t length)
+{
+    size_t  i;
+
+    i = 0;
+    // Loop through the array of numbers
+    while (i < length)
+    {
+        // Check if the current number is even
+        if (numbers[i] % 2 == 0)
+        {
+            // Print the number if it is even
+            printf("%d\n", numbers[i]);
+        }
+        i++;
+    }
+}
+
+void print_even(int *numbers, size_t length)
+{
+    size_t  i;
+
+    i = 0;
+    // Print even numbers from the array
+    while (i < length)
+    {
+        if (numbers[i] % 2 == 0)
+            printf("%d\n", numbers[i]);
+        i++;
+    }
+}
